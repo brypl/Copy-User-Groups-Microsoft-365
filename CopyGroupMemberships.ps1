58a41e5a441eafd582ce888687a5816c6323e113
Bryan Lam <61400370+brypl@users.noreply.github.com>
2021-08-11 15:13:27 +1000

    Create CopyGroupMemberships.ps1

---
+++ CopyGroupMemberships.ps1
@@ -0,0 +1,15 @@
+﻿import-module AzureAD
+
+Connect-AzureAD
+
+$copyuser = Read-Host "Enter the email of the user you want to copy "
+$newuser = Read-Host "Enter the email of the user you want groups copied to "
+$usermemberships = Get-AzureADUsermembership -ObjectId $copyuser 
+$newuserRefObjectID = Get-AzureADUser -ObjectId $newuser
+
+
+foreach ($groups in $usermemberships.ObjectID) {
+
+Add-AzureADGroupMember -ObjectId $groups -RefObjectId $newuserRefObjectID.ObjectId
+
+}
